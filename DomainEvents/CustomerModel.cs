987e7e596bb1903dbe7416d239645f855b89e276
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-09-30 16:00:14 +0330

    Add CustomerModel

---
+++ DomainEvents/CustomerModel.cs
@@ -0,0 +1,23 @@
+﻿namespace DomainEvents
+{
+    public class CustomerModel : DomainEntity
+    {
+        public int CustomerId { get; set; }
+        public bool IsPreferred { get; set; }
+
+        public void Preferred()
+        {
+            if (!IsPreferred)
+            {
+                IsPreferred = true;
+
+                RaiseEvent(new CustomerIsPreferred { CustomerId = CustomerId });
+            }
+        }
+    }
+
+    public class CustomerIsPreferred : IEvent
+    {
+        public int CustomerId { get; set; }
+    }
+}
