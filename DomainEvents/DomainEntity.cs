8d474f90a074999e082d195054b412352c85bfa8
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-09-30 15:50:05 +0330

    Add Domain Entity

--- DomainEvents/DomainEntity.cs
+++ DomainEvents/DomainEntity.cs
@@ -2,9 +2,19 @@
 {
     public interface IDomainEntity
     {
+        void RaiseEvent(IEvent @event);
+        IEnumerable<IEvent> GetEvents();
+        void ClearEvents();
     }
-    
+
     public abstract class DomainEntity : IDomainEntity
     {
+        private readonly List<IEvent> _events = new();
+
+        public void RaiseEvent(IEvent @event) => _events.Add(@event);
+
+        public IEnumerable<IEvent> GetEvents() => _events;
+
+        public void ClearEvents() => throw new NotImplementedException();
     }
 }
